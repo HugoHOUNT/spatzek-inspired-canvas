e14cc0b680288e292f77b8bafccacf8c559eaf04
HugoHOUNTesgi <hhountondji1@myges.fr>
2025-04-16 00:08:58 +0200

    Ajout projets 1 - Modifs project.ts technologies et autres

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,15 +7,15 @@ export const projectsData: Project[] = [
     title: "Power BI de la Scolarité",
     description: "Afin de mieux piloter les parcours de certification et de diplomation, j'ai développé un outil de visualisation de données sous Power BI, intégrant l'ensemble des indicateurs clés des jurys. Ce projet m'a permis de transformer des données brutes en un levier stratégique d'aide à la décision.",
     category: "Data Visualisation",
-    technologies: ["Power BI", "DAX", "Power Query", "SQL Server"],
+    technologies: ["Power BI", "DAX", "Power Query"],
     year: 2023,
     involvement: "Projet solo",
     status: "Terminé",
     image: "/lovable-uploads/1abc8b8d-d6d6-4109-b050-4085f39e80b2.png",
     details: {
-      context: "Dans le cadre des parcours de certification et de diplomation proposés par IFG Executive Education, le processus de gestion des candidatures est complexe et implique plusieurs étapes cruciales : vérification de la conformité des dossiers, validation par des jurys successifs, et suivi des différents statuts administratifs des candidats.",
-      objective: "Centraliser et visualiser de manière dynamique l'ensemble des données liées aux jurys de diplomation et de certification, offrant une lecture immédiate et intuitive des performances des formations.",
-      impact: "Cet outil est devenu un levier stratégique pour la direction pédagogique et les équipes opérationnelles, améliorant la prise de décision et l'efficacité opérationnelle.",
+      context: "Dans le cadre des parcours de certification et de diplomation proposés par IFG Executive Education, la gestion des candidatures constitue un processus complexe et structurant. Elle implique plusieurs étapes clés telles que la vérification de la conformité des dossiers, la validation par des jurys successifs, ainsi que le suivi administratif de chaque candidat. Face à la diversité des programmes, à l’augmentation des flux de candidatures et à la multiplicité des étapes, la gestion manuelle de ces processus devenait de plus en plus difficile. Cette dispersion des données entravait non seulement la lisibilité des parcours, mais ralentissait aussi la prise de décision.",
+      objective: "Afin de répondre à ces enjeux, j’ai conçu et développé un tableau de bord interactif sous Power BI. L’objectif était de centraliser l’ensemble des données relatives aux jurys de certification et de diplomation, de les rendre intelligibles visuellement et de les transformer en un véritable outil d’aide à la décision pour la direction pédagogique et les équipes opérationnelles. Le Power BI permet ainsi de : \n -Suivre l’état d’avancement des candidatures, \n -Identifier les dossiers non conformes, \n -Mesurer les délais de traitement et les performances des jurys \n -Visualiser les abandons, reports, certifications et taux de réussite par programme, /Anticiper les besoins d’accompagnement et planifier efficacement les prochaines sessions.",
+      impact: "Ce tableau de bord est rapidement devenu un levier stratégique pour l’organisation : \n -Il renforce la transparence des processus de certification, \n -Il améliore la réactivité des équipes et l’efficacité opérationnelle, \n -Il permet d’anticiper les risques d’abandon et d’échec, en facilitant l’identification des points de blocage, \n -Il valorise l’expérience des apprenants en assurant un meilleur suivi qualitatif des parcours, \n -Il soutient la réflexion stratégique sur l’évolution des programmes et des flux de candidats. Il contribue directement à la valorisation de l’expérience apprenant et à la réputation de l’établissement sur le marché de la formation professionnelle.",
       moreImages: [
         "/lovable-uploads/b9406aa6-82d8-46da-b733-ffa2e2320364.png",
         "/lovable-uploads/846f45e2-6833-4302-ac33-eba1d93f8ddf.png",
@@ -25,6 +25,29 @@ export const projectsData: Project[] = [
         "/lovable-uploads/a81ce782-8671-4c8d-99a6-46c59dbf7722.png",
         "/lovable-uploads/7a4cf249-cea2-4115-bcc0-b3c10d9a652d.png",
         "/lovable-uploads/6e7bcd73-d72a-471b-a459-cc8ff8864425.png"
+      ]
+    }
+  },
+  {
+    id: 2,
+    title: "Tableau de bord des Admissions",
+    description: "Afin d’optimiser la gestion du processus d’admission, j’ai conçu un tableau de bord Power BI centralisant les indicateurs clés de suivi des candidatures. Ce projet m’a permis de transformer des données opérationnelles en un outil d’aide à la décision, facilitant le pilotage des performances et l’amélioration continue de l’expérience candidat.",
+    category: "Business Intelligence / Data Visualisation / Pilotage des processus",
+    technologies: ["Power BI Desktop", "Power Query (ETL)", "DAX", "Sharepoint", "Excel"],
+    year: 2022,
+    involvement: "Travail d\'équipe",
+    status: "Terminé",
+    image: "",
+    details: {
+      context: "Dans le cadre de ses activités de formation professionnelle, l’établissement reçoit un flux important de candidatures pour ses différents programmes. Chaque parcours implique plusieurs étapes de validation – vérification de la conformité, passage en jury, décision finale. Cette complexité, accentuée par la diversité des profils, nécessitait un outil fiable et flexible pour suivre en temps réel l’avancement des dossiers et optimiser les ressources.",
+      objective: "Le but de ce projet était de :\n\n- Centraliser toutes les données du processus d’admission\n- Optimiser les délais de traitement des dossiers\n- Améliorer l’expérience des candidats en assurant un meilleur suivi\n- Offrir aux équipes un outil d’analyse opérationnelle et stratégique pour piloter les performances d’admission",
+      impact: "Ce tableau de bord a eu un impact significatif :\n\n- Vision globale et instantanée de l’état d’avancement des candidatures\n- Identification rapide des goulots d’étranglement (ex : délais dans les jurys)\n- Pilotage individualisé par consultant, programme ou période\n- Amélioration de la réactivité et coordination des équipes\n- Contribution directe à l’atteinte des objectifs d’admission et à la satisfaction des apprenants\n- Renforcement de la transparence et appui à la prise de décision managériale",
+      moreImages: [
+        "", // Ajouter ici les URLs des 5 captures d'écran
+        "",
+        "",
+        "",
+        ""
       ]
     }
   },
