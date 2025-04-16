108825aaba834a7a3e4755c4ecca5939eaeb86d4
HugoHOUNTesgi <hhountondji1@myges.fr>
2025-04-16 22:26:22 +0200

    Modification de l'emplacement des images

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -11,20 +11,21 @@ export const projectsData: Project[] = [
     year: 2023,
     involvement: "Projet solo",
     status: "Terminé",
-    image: "/lovable-uploads/1abc8b8d-d6d6-4109-b050-4085f39e80b2.png",
+    image: "/assets/images/Scolarite/Scol1.png",
     details: {
       context: "Dans le cadre des parcours de certification et de diplomation proposés par IFG Executive Education, la gestion des candidatures constitue un processus complexe et structurant. Elle implique plusieurs étapes clés telles que la vérification de la conformité des dossiers, la validation par des jurys successifs, ainsi que le suivi administratif de chaque candidat. Face à la diversité des programmes, à l’augmentation des flux de candidatures et à la multiplicité des étapes, la gestion manuelle de ces processus devenait de plus en plus difficile. Cette dispersion des données entravait non seulement la lisibilité des parcours, mais ralentissait aussi la prise de décision.",
       objective: "Afin de répondre à ces enjeux, j’ai conçu et développé un tableau de bord interactif sous Power BI. L’objectif était de centraliser l’ensemble des données relatives aux jurys de certification et de diplomation, de les rendre intelligibles visuellement et de les transformer en un véritable outil d’aide à la décision pour la direction pédagogique et les équipes opérationnelles. Le Power BI permet ainsi de : \n -Suivre l’état d’avancement des candidatures, \n -Identifier les dossiers non conformes, \n -Mesurer les délais de traitement et les performances des jurys \n -Visualiser les abandons, reports, certifications et taux de réussite par programme, /Anticiper les besoins d’accompagnement et planifier efficacement les prochaines sessions.",
       impact: "Ce tableau de bord est rapidement devenu un levier stratégique pour l’organisation : \n -Il renforce la transparence des processus de certification, \n -Il améliore la réactivité des équipes et l’efficacité opérationnelle, \n -Il permet d’anticiper les risques d’abandon et d’échec, en facilitant l’identification des points de blocage, \n -Il valorise l’expérience des apprenants en assurant un meilleur suivi qualitatif des parcours, \n -Il soutient la réflexion stratégique sur l’évolution des programmes et des flux de candidats. Il contribue directement à la valorisation de l’expérience apprenant et à la réputation de l’établissement sur le marché de la formation professionnelle.",
       moreImages: [
-        "/lovable-uploads/b9406aa6-82d8-46da-b733-ffa2e2320364.png",
-        "/lovable-uploads/846f45e2-6833-4302-ac33-eba1d93f8ddf.png",
-        "/lovable-uploads/e59f1dd3-1cf6-4a01-8428-4b4c851b0b3a.png",
-        "/lovable-uploads/9be17574-27e0-4d8d-86b8-475851ff7cce.png",
-        "/lovable-uploads/fc883283-d3fa-4daa-b61a-da2aa458b7ac.png",
-        "/lovable-uploads/a81ce782-8671-4c8d-99a6-46c59dbf7722.png",
-        "/lovable-uploads/7a4cf249-cea2-4115-bcc0-b3c10d9a652d.png",
-        "/lovable-uploads/6e7bcd73-d72a-471b-a459-cc8ff8864425.png"
+        "/assets/images/Scolarite/Scol1.png",
+        "/assets/images/Scolarite/Scol2.png",
+        "/assets/images/Scolarite/Scol3.png",
+        "/assets/images/Scolarite/Scol4.png",
+        "/assets/images/Scolarite/Scol5.png",
+        "/assets/images/Scolarite/Scol6.png",
+        "/assets/images/Scolarite/Scol7.png",
+        "/assets/images/Scolarite/Scol8.png",
+        "/assets/images/Scolarite/Scol9.png"
       ]
     }
   },
@@ -37,17 +38,17 @@ export const projectsData: Project[] = [
     year: 2024,
     involvement: "Travail d\'équipe",
     status: "Terminé",
-    image: "/lovable-uploads/2aa7fcd3-f48b-4925-9517-aff9ad505d64.png",
+    image: "/assets/images/Admissions/Admissions1.png",
     details: {
       context: "Dans le cadre de ses activités de formation professionnelle, l'établissement reçoit un flux important de candidatures pour ses différents programmes. Chaque parcours implique plusieurs étapes de validation – vérification de la conformité, passage en jury, décision finale. Cette complexité, accentuée par la diversité des profils, nécessitait un outil fiable et flexible pour suivre en temps réel l'avancement des dossiers et optimiser les ressources.",
       objective: "Le but de ce projet était de :\n\n- Centraliser toutes les données du processus d'admission\n- Optimiser les délais de traitement des dossiers\n- Améliorer l'expérience des candidats en assurant un meilleur suivi\n- Offrir aux équipes un outil d'analyse opérationnelle et stratégique pour piloter les performances d'admission",
       impact: "Ce tableau de bord a eu un impact significatif :\n\n- Vision globale et instantanée de l'état d'avancement des candidatures\n- Identification rapide des goulots d'étranglement (ex : délais dans les jurys)\n- Pilotage individualisé par consultant, programme ou période\n- Amélioration de la réactivité et coordination des équipes\n- Contribution directe à l'atteinte des objectifs d'admission et à la satisfaction des apprenants\n- Renforcement de la transparence et appui à la prise de décision managériale",
       moreImages: [
-        "/lovable-uploads/2aa7fcd3-f48b-4925-9517-aff9ad505d64.png",
-        "/lovable-uploads/d3d1c1ff-d8d7-44d3-960f-50b7da5c0343.png",
-        "/lovable-uploads/0923f3c5-df47-4f36-9213-e78ed126b441.png",
-        "/lovable-uploads/9f9d48d1-31b0-4ebe-b4f3-35ad922b1a84.png",
-        "/lovable-uploads/0687ec17-394f-4e77-a664-c074a7e373a5.png",
+        "/assets/images/Admissions/Admissions1.png",
+        "/assets/images/Admissions/Admissions2.png",
+        "/assets/images/Admissions/Admissions3.png",
+        "/assets/images/Admissions/Admissions4.png",
+        "/assets/images/Admissions/Admissions5.png",
       ]
     }
   },
@@ -60,22 +61,22 @@ export const projectsData: Project[] = [
     year: 2025,
     involvement: "Travail d\'équipe",
     status: "En cours",
-    image: "",
+    image: "/assets/images/SessionsPlanif/Admissions1.png",
     details: {
       context: "L’organisation gère un portefeuille large et diversifié de sessions de formation, réparties sur plusieurs sites administratifs, thématiques et catégories de programmes. Cette diversité rendait complexe le suivi en temps réel de la planification, de l'exécution et de la performance des sessions. Il devenait nécessaire de centraliser ces données dans un outil unique et interactif afin d'améliorer la coordination et d'aligner les efforts avec les objectifs pédagogiques et stratégiques.",
       objective: "L’objectif principal était de :\n\n- Centraliser les données liées à la création, au suivi et à l’exécution des sessions de formation\n- Permettre une comparaison directe entre les objectifs fixés et les réalisations\n- Optimiser la gestion des ressources humaines (intervenants, prestataires) et logistiques\n- Fournir une interface claire pour appuyer les décisions opérationnelles et stratégiques",
       impact: "Ce tableau de bord a eu un impact notable :\n\n- Vision consolidée et en temps réel de toutes les sessions, par site, programme ou période\n- Suivi détaillé des objectifs atteints et des écarts observés\n- Amélioration de l’allocation des ressources (formateurs, intervenants)\n- Meilleure anticipation des besoins logistiques et humains\n- Réduction des écarts entre planification et exécution\n- Renforcement de la coordination interservices et amélioration de la réactivité globale",
       moreImages: [
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        ""
+        "/assets/images/SessionsPlanif/Admissions1.png",
+        "/assets/images/SessionsPlanif/Admissions2.png",
+        "/assets/images/SessionsPlanif/Admissions3.png",
+        "/assets/images/SessionsPlanif/Admissions4.png",
+        "/assets/images/SessionsPlanif/Admissions5.png",
+        "/assets/images/SessionsPlanif/Admissions6.png",
+        "/assets/images/SessionsPlanif/Admissions7.png",
+        "/assets/images/SessionsPlanif/Admissions8.png",
+        "/assets/images/SessionsPlanif/Admissions9.png",
+        "/assets/images/SessionsPlanif/Admissions10.png"
       ]
     }
   },
@@ -88,19 +89,19 @@ export const projectsData: Project[] = [
     year: 2024,
     involvement: "Travail d\'équipe",
     status: "Terminé",
-    image: "",
+    image: "/assets/images/AmeliorationContinue/AmeliorationContinue1.png",
     details: {
       context: "Dans une dynamique de qualité et d'amélioration continue, il est essentiel pour une organisation de suivre efficacement ses actions correctives, préventives et d'amélioration. Les exigences des normes de certification, ainsi que les attentes accrues des parties prenantes internes et externes, imposent une rigueur dans la gestion de ces processus. Face à la dispersion des informations dans différents outils et formats, le besoin d’une solution centralisée de pilotage est devenu évident.",
       objective: "L’objectif de ce projet est double :\n\n1. Piloter efficacement la performance qualité en garantissant un suivi précis des actions d'amélioration.\n2. Faciliter la communication transversale entre les équipes, en rendant l’information accessible et compréhensible de tous.\n\nCela contribue à renforcer la culture d’amélioration continue au sein de l’organisation.",
       impact: "Ce tableau de bord a permis :\n\n- Une centralisation de toutes les actions d’amélioration (correctives, préventives, satisfaction...)\n- Une meilleure traçabilité des actions et de leurs responsables\n- Un suivi visuel et intuitif de l’état d’avancement, par origine ou type de demande\n- Une priorisation claire des actions à traiter\n- Une réactivité accrue des équipes qualité et opérationnelles\n- Une meilleure préparation aux audits qualité grâce à une traçabilité rigoureuse",
       moreImages: [
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        ""
+        "/assets/images/AmeliorationContinue/AmeliorationContinue1.png",
+        "/assets/images/AmeliorationContinue/AmeliorationContinue2.png",
+        "/assets/images/AmeliorationContinue/AmeliorationContinue3.png",
+        "/assets/images/AmeliorationContinue/AmeliorationContinue4.png",
+        "/assets/images/AmeliorationContinue/AmeliorationContinue5.png",
+        "/assets/images/AmeliorationContinue/AmeliorationContinue6.png",
+        "/assets/images/AmeliorationContinue/AmeliorationContinue7.png"
       ]
     }
   },
@@ -113,14 +114,19 @@ export const projectsData: Project[] = [
     year: 2024,
     involvement: "Travail d\'équipe",
     status: "Terminé",
-    image: "",
+    image: "/assets/images/EffectifApprenants/EffectifApprenant1.png",
     details: {
       context: "Le suivi des effectifs est un enjeu central pour garantir un bon dimensionnement des ressources pédagogiques. Jusqu’alors dispersées dans plusieurs fichiers, les données nécessitaient d’être consolidées dans un outil unique.",
       objective: "Permettre :\n- Une analyse dynamique des volumes d’apprenants\n- Une projection à moyen terme des flux d’inscription\n- Une vision par programme, statut, promo et mois",
       impact: "- Amélioration de la prévision RH et logistique\n- Visualisation claire des tendances d’inscriptions\n- Aide à la planification stratégique des formations",
       moreImages: [
-        "",
-        ""
+        "/assets/images/EffectifApprenants/EffectifApprenant1.png",
+        "/assets/images/EffectifApprenants/EffectifApprenant2.png",
+        "/assets/images/EffectifApprenants/EffectifApprenant3.png",
+        "/assets/images/EffectifApprenants/EffectifApprenant4.png",
+        "/assets/images/EffectifApprenants/EffectifApprenant5.png",
+        "/assets/images/EffectifApprenants/EffectifApprenant6.png",
+        "/assets/images/EffectifApprenants/EffectifApprenant7.png"
       ]
     }
   },
@@ -133,14 +139,16 @@ export const projectsData: Project[] = [
     year: 2024,
     involvement: "Travail d\'équipe",
     status: "Terminé",
-    image: "",
+    image: "/assets/images/Marge/Marge1.png",
     details: {
       context: "Le suivi économique des formations était jusque-là morcelé entre plusieurs outils. Il devenait indispensable de disposer d’un tableau de bord consolidé pour analyser les marges générées par programme et session.",
       objective: "Mesurer précisément :\n- La marge brute par session\n- Les postes de dépense les plus lourds\n- Le ratio inscription/coût\n- L’évolution des marges par année et programme",
       impact: "- Vision stratégique de la rentabilité\n- Optimisation des coûts\n- Pilotage précis des ressources engagées\n- Appui aux décisions d’investissement ou de repositionnement",
       moreImages: [
-        "",
-        ""
+        "/assets/images/Marge/Marge1.png",
+        "/assets/images/Marge/Marge2.png",
+        "/assets/images/Marge/Marge3.png",
+        "/assets/images/Marge/Marge4.png",
       ]
     }
   },
@@ -153,14 +161,18 @@ export const projectsData: Project[] = [
     year: 2024,
     involvement: "Travail d\'équipe",
     status: "Terminé",
-    image: "",
+    image: "/assets/images/SatisfactionClient/Satisfaction1.png",
     details: {
       context: "L’entreprise souhaitait unifier les données issues des enquêtes de satisfaction post-formation afin de suivre les retours des apprenants et identifier les points forts et axes d’amélioration.",
       objective: "Créer un tableau de bord permettant de :\n- Suivre la satisfaction client sur plusieurs axes\n- Mesurer l’évolution des indicateurs dans le temps\n- Identifier les modules les mieux perçus ou critiques",
       impact: "- Approche proactive d’amélioration continue\n- Vision claire des ressentis apprenants\n- Réactivité accrue des équipes pédagogiques\n- Valorisation des retours lors d’audits ou de présentations internes",
       moreImages: [
-        "",
-        ""
+        "/assets/images/SatisfactionClient/Satisfaction1.png",
+        "/assets/images/SatisfactionClient/Satisfaction2.png",
+        "/assets/images/SatisfactionClient/Satisfaction3.png",
+        "/assets/images/SatisfactionClient/Satisfaction4.png",
+        "/assets/images/SatisfactionClient/Satisfaction5.png",
+        "/assets/images/SatisfactionClient/Satisfaction6.png"
       ]
     }
   },
@@ -169,19 +181,20 @@ export const projectsData: Project[] = [
     title: "Power BI – Fréquentation Hôtelière et Touristique",
     description: "Ce tableau de bord permet de visualiser en un clin d’œil qui vient, quand, pourquoi et où, pour aider à piloter intelligemment le tourisme et l’hôtellerie.",
     category: "Data Visualisation",
-    technologies: ["Power BI Desktop", "Power Query (ETL)", "DAX", "Excel"],
+    technologies: ["Power BI Desktop", "Power Query (ETL)", "DAX"],
     year: 2025,
     involvement: "Travail d\'équipe",
     status: "Terminé",
-    image: "",
+    image: "/assets/images/FrequentationHoteliere/FrequentationHoteliere1.png",
     details: {
       context: "Dans un contexte de relance touristique post-COVID, il était essentiel de mieux comprendre les flux de voyageurs, leurs profils, motivations et zones d’accueil afin d’adapter les politiques territoriales.",
       objective: "Permettre aux acteurs publics et privés de :\n- Suivre la fréquentation par période, zone et typologie\n- Identifier les clientèles à forte valeur\n- Anticiper les besoins en hébergement et services",
       impact: "- Vision stratégique du tourisme en Polynésie\n- Soutien aux décisions d’investissement\n- Analyse des tendances émergentes (écotourisme, croisières de luxe…)\n- Renforcement de l’attractivité du territoire",
       moreImages: [
-        "",
-        "",
-        ""
+        "/assets/images/FrequentationHoteliere/FrequentationHoteliere1.png",
+        "/assets/images/FrequentationHoteliere/FrequentationHoteliere2.png",
+        "/assets/images/FrequentationHoteliere/FrequentationHoteliere3.png",
+        "/assets/images/FrequentationHoteliere/FrequentationHoteliere4.png"
       ]
     }
   },
